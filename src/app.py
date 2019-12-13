c681d2a76060a8688cdb1e872717612eb74d7da2
Jasman <jasmansingh84@gmail.com>
2019-12-12 21:00:52 -0500

    Feature: Import db and bcrypt from src/model.

--- src/app.py
+++ src/app.py
@@ -5,6 +5,12 @@ from flask import Flask
 from .config import app_config
 
 
+from flask import Flask
+
+from .config import app_config
+from .models import db, bcrypt  # add this new line
+
+
 def create_app(env_name):
     """
     Create app
@@ -21,5 +27,27 @@ def create_app(env_name):
         example endpoint
         """
         return 'Congratulations! Your first endpoint is workin'
+
+    return app
+
+
+# src/app.py
+
+
+
+def create_app(env_name):
+
+
+    # app initiliazation
+    app = Flask(__name__)
+
+    app.config.from_object(app_config[env_name])
+
+    # initializing bcrypt
+    bcrypt.init_app(app)  # add this line
+
+    db.init_app(app)  # add this line
+
+
 
     return app
