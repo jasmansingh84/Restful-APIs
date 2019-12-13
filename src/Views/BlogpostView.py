9d385fcf04d6d1a7055504a481fb4a23218de9d8
Jasman <jasmansingh84@gmail.com>
2019-12-12 23:28:44 -0500

    Feature: Added Get all blogpost endpoint.

--- src/Views/BlogpostView.py
+++ src/Views/BlogpostView.py
@@ -34,3 +34,37 @@ def custom_response(res, status_code):
     response=json.dumps(res),
     status=status_code
   )
+
+
+#/src/views/BlogpostView.py
+# app initiliazation
+#####################
+# existing code remain #
+######################
+
+
+
+  """
+  Create Blogpost Function
+  """
+  # app initiliazation
+  #####################
+  # existing code remain #
+  ######################
+  return custom_response(data, 201)
+
+# add this function
+@blogpost_api.route('/', methods=['GET'])
+def get_all():
+  """
+  Get All Blogposts
+  """
+  posts = BlogpostModel.get_all_blogposts()
+  data = blogpost_schema.dump(posts, many=True).data
+  return custom_response(data, 200)
+
+
+# app initiliazation
+#####################
+# existing code remain #
+######################
